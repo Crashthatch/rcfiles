d18b8b779686ee331a5b6982d652d307947dfcdc
Tom Fletcher <fletcher.tom@gmail.com>
2017-06-24 21:55:06 +0100

    More packages

--- install-apt-packages.sh
+++ install-apt-packages.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y aws-cli vim htop iotop dnsutils git
+sudo apt-get install -y awscli vim htop iotop dnsutils git python-pip mtr
 
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+
+#docker-compose:
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 
