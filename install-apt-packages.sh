9fadc2fba35e452e78369c15afe8454fffc08ef9
Tom Fletcher <fletcher.tom@gmail.com>
2017-06-24 21:37:26 +0100

    Add script that provisions apt-packages etc.

---
+++ install-apt-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y aws-cli vim htop iotop dnsutils git
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
